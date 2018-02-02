27305db49c7dcf8ca3a9ec7e1d4ab026fba37dcc
Will Pease <william.pease@maine.edu>
2018-02-01 20:32:03 -0500

    qsize

--- src/QtCore/qsize.h
+++ src/QtCore/qsize.h
@@ -38,7 +38,7 @@
 class QSizeWrap : public node::ObjectWrap {
  public:
   static void Initialize(v8::Handle<v8::Object> target);
-  static v8::Local<Value> NewInstance(const v8::FunctionCallbackInfo<v8::Value>& args, QSize q);
+  static v8::Local<v8::Value> NewInstance(const v8::FunctionCallbackInfo<v8::Value>& args, QSize q);
   QSize* GetWrapped() const { return q_; };
   void SetWrapped(QSize q) { 
     if (q_) delete q_; 
